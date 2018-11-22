ab604d360581a07795a5e3e04340606641f598ae
Jacob LeBerth <jtl4027@rit.edu>
2018-11-21 19:24:10 -0500

    First Commit
    First Upload of SatchelCreatures Beginning.
    Created using JesseEtzler's "Lets clone a game" Pokemon Tutorial which can be found here.
    https://www.youtube.com/playlist?list=PLxLNqnnCshwmIv7OsOZ08dZEVvPdbrrnz

---
+++ SatchelCree/Assets/Scripts/Main.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+	//All Phantoms
+	[SerializeField]
+	public List<Phantom> allPhantoms;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
+
+[System.Serializable]
+public class Phantom
+{
+	
+	public Type type;
+
+	public string name;
+	public string nickName;
+
+	public float baseHealth;
+	public float currentHealth;
+
+	public float basePower;
+	public float currentPower;
+
+	public float baseDefense;
+	public float currentDefense;
+
+	public float baseAgility;
+	public float currentAgility;
+
+}
+
+public enum Type
+{
+	earth,
+	wind,
+	fire,
+	water
+}
---
+++ SatchelCree/Assets/Scripts/Player.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public Vector3 startpoint;
+	public Vector3 endpoint;
+	public float speed;
+	private float increment;
+	public bool isMoving;
+	public bool isInCombat;
+
+	//cameras
+	public GameObject CameraMain;
+	public GameObject CombatCamera;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		CombatCamera.SetActive(false);
+		startpoint = transform.position;
+		endpoint = transform.position;
+		isInCombat = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(increment <=1 && isMoving==true)
+		{
+			increment += speed / 100;
+		}
+		else
+		{
+			isMoving = false;
+		}
+
+		if(isMoving)
+		{
+			transform.position = Vector3.Lerp(startpoint, endpoint, increment);
+		}
+
+		if (!isInCombat)
+		{
+			Move();
+			if (Input.GetKeyDown("c"))
+			{
+				EnterCombat();
+			}
+		}
+	}
+
+	/// <summary>
+	/// Callable method to tell the player that combat has been initiated
+	/// </summary>
+	public void EnterCombat()
+	{
+		isInCombat = true;
+		CameraMain.SetActive(false);
+		CombatCamera.SetActive(true);
+	}
+
+	/// <summary>
+	/// Takes user input and moves around screen.
+	/// </summary>
+	public void Move()
+	{
+		if (Input.GetKey("w") && isMoving == false)
+		{
+			bool disableMove = false; ;
+			RaycastHit hit;
+			if(Physics.Raycast (transform.position, Vector3.forward, out hit, 1))
+			{
+				float distancetoGround = hit.distance;
+
+				if (hit.collider.gameObject.tag == "tree")
+				{
+					disableMove = true;
+				}
+			}
+
+			if (!disableMove)
+			{
+				increment = 0;
+				isMoving = true;
+				startpoint = transform.position;
+				endpoint = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+			}
+		}
+		else if (Input.GetKey("s") && isMoving == false)
+		{
+			bool disableMove = false; ;
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, Vector3.forward * -1, out hit, 1))
+			{
+				float distancetoGround = hit.distance;
+
+				if (hit.collider.gameObject.tag == "tree")
+				{
+					disableMove = true;
+				}
+			}
+
+			if (!disableMove)
+			{
+				increment = 0;
+				isMoving = true;
+				startpoint = transform.position;
+				endpoint = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+			}
+		}
+		else if (Input.GetKey("a") && isMoving == false)
+		{
+			bool disableMove = false; ;
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, Vector3.right * -1, out hit, 1))
+			{
+				float distancetoGround = hit.distance;
+
+				if (hit.collider.gameObject.tag == "tree")
+				{
+					disableMove = true;
+				}
+			}
+
+			if (!disableMove)
+			{
+				increment = 0;
+				isMoving = true;
+				startpoint = transform.position;
+				endpoint = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+			}
+		}
+		else if (Input.GetKey("d") && isMoving == false)
+		{
+			bool disableMove = false; ;
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, Vector3.right, out hit, 1))
+			{
+				float distancetoGround = hit.distance;
+
+				if (hit.collider.gameObject.tag == "tree")
+				{
+					disableMove = true;
+				}
+			}
+
+			if (!disableMove)
+			{
+				increment = 0;
+				isMoving = true;
+				startpoint = transform.position;
+				endpoint = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+			}
+		}
+	}
+}
